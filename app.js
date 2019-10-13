89c7b5d31bc750c2ba2087c2e12c04b4cf35c337
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-10-13 23:30:38 +1300

    Initialise mongoose

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+//Initiate mongoDB connection using database specified in environment variable
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Database Schema
+const serverSchema = new mongoose.Schema({
+	serverID: String
+});
+
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -16,5 +25,5 @@ client.on('message', message => {
 	}
 });
 
-//Login Bot
+//Login Bot using token from environment variable
 client.login(process.env.TOKEN);
