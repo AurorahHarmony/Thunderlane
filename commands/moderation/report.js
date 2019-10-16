6cc093b93eb717141309c7c63694643c84df305c
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-10-16 14:59:49 +1300

    Create report command

---
+++ commands/moderation/report.js
@@ -0,0 +1,35 @@
+const { RichEmbed } = require('discord.js');
+const { stripIndents } = require('common-tags');
+
+module.exports = {
+	name: 'report',
+	category: 'moderation',
+	description: 'Reports a member',
+	usage: '<mention> | <id>',
+	run: async (client, message, args) => {
+		if (message.deletable) message.delete();
+
+		let rMember = message.mentions.members.first() || message.guild.members.get(args[0]);
+
+		if (!rMember) return message.reply('Couldnt find that person').then(m => m.delete(client.config.liveTime));
+
+		if (rMember.hasPermission('BAN_MEMBERS') || rMember.user.bot) return message.reply('Cannot report that member').then(m => m.delete(client.config.liveTime));
+
+		if (!args[1]) return message.channel.send('Please provide a reason for the report').then(m => m.delete(client.config.liveTime));
+
+		const channel = message.guild.channels.find(channel => channel.name === 'reports');
+
+		if (!channel) return message.channel.send('I could not find the log channel').then(m => m.delete(client.config.liveTime));
+
+		const embed = new RichEmbed()
+			.setColor(client.config.color.error)
+			.setTimestamp()
+			.setFooter(message.guild.name, message.guild.iconURL)
+			.setAuthor('Reported Member', rMember.user.displayAvatarURL).setDescription(stripIndents`**Member:** ${rMember} (${rMember.id})
+      **Reported By:** ${message.member} (${message.member.id})
+      **Reported In:** ${message.channel}
+      **Reason:** ${args.slice(1).join(' ')}`);
+
+		return channel.send(embed);
+	}
+};
