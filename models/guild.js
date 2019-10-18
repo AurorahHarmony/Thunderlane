a7fa616294c8a986bcf9bdfea794d0e3b6f337d9
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-10-18 22:34:05 +1300

    Extend config schema

--- models/guild.js
+++ models/guild.js
@@ -5,7 +5,29 @@ const guildSchema = new mongoose.Schema({
 	guildID: { type: String, required: true },
 	guildName: String,
 	prefix: String,
-	logChannel: String
+	logChannels: {
+		reports: {
+			enabled: Boolean,
+			channel: String
+		},
+		modLog: {
+			enabled: Boolean,
+			channel: String
+		},
+		joinLog: {
+			enabled: Boolean,
+			channel: String
+		},
+		leaveLog: {
+			enabled: Boolean,
+			channel: String
+		},
+		wordWhitelist: {
+			enabled: Boolean,
+			channel: String,
+			ignoredMembers: Array
+		}
+	}
 });
 
 guildSchema.plugin(findOrCreate);
