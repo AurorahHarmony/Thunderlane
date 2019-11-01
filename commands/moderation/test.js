181f229dbb2c0ffbee3857c2b19e124154b468f0
Aurorah Harmony <aurorahHarmony@gmail.com>
2019-11-01 23:04:45 +1300

    Empty testing command

--- commands/moderation/test.js
+++ commands/moderation/test.js
@@ -7,29 +7,5 @@ module.exports = {
 	category: 'moderation',
 	description: 'softbans the member',
 	usage: '<id | mention>',
-	run: async (client, message, args) => {
-		let toSoftBan = {
-			userID: message.author.id,
-			unbanTime: getUnixTime() + 100,
-			bannedTime: getUnixTime()
-		};
-
-		let softBanIndex = client.foundGuild.softBans.findIndex(current => current.userID === toSoftBan.userID);
-
-		if (softBanIndex === -1) {
-			client.foundGuild.softBans.push(toSoftBan);
-		} else {
-			client.foundGuild.softBans[softBanIndex] = toSoftBan;
-		}
-
-		console.log(client.foundGuild.softBans);
-
-		// client.foundGuild.save();
-		// console.log(client.foundGuild.softBans);
-
-		// console.log(client.foundGuild.softBans.find(usr => usr.userID === '433558945873788928' || usr.userID === `<@${toBan}>`));
-		// console.log(client.foundGuild.softBans.indexOf(usr => usr.userID === '373931417563824100'));
-		// console.log(client.foundGuild);
-		// client.foundGuild.doc.xp.set(client.foundGuild.guildID, 5);
-	}
+	run: async (client, message, args) => {}
 };
